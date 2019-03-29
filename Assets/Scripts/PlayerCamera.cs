8df48675b47d621942fc71433e37d29057534a4e
Ross Massey <rmassey@ucsd.edu>
2019-03-29 16:36:20 -0700

    Add player look

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Controls player look
+ * 
+ * Thanks to Holistic3d on YouTube for their tutorial
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    public float cameraSensitivity = 5f;
+    public float cameraSmoothing = 2f;
+
+    float cameraScale;
+
+    Vector2 mouseLook;
+    Vector2 smoothVector;
+
+    Transform playerTransform;
+
+    void Start()
+    {
+        cameraScale = cameraSensitivity * cameraSmoothing;
+        playerTransform = this.transform.parent.gameObject.transform;
+    }
+
+    void Update()
+    {
+        CalculatMouseLook();
+        ApplyTransforms();
+    }
+
+    private void ApplyTransforms()
+    {
+        // rotate camera up and down
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        // rotate player left and right
+        playerTransform.localRotation = Quaternion.AngleAxis(mouseLook.x, playerTransform.up);
+    }
+
+    private void CalculatMouseLook()
+    {
+        Vector2 mouseChange = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseChange = Vector2.Scale(mouseChange, new Vector2(cameraScale, cameraScale));
+        smoothVector.x = Mathf.Lerp(smoothVector.x, mouseChange.x, 1f / cameraSmoothing);
+        smoothVector.y = Mathf.Lerp(smoothVector.y, mouseChange.y, 1f / cameraSmoothing);
+        mouseLook += smoothVector;
+    }
+}
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,27 +7,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-
     public float playerSpeed = 15f;
 
-    // Start is called before the first frame update
     void Start()
     {
         // lock cursor to window and hide
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float verticalMovement = 
-            Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
-        float horizontalMovement = 
-            Input.GetAxis("Horizontal") * playerSpeed  * Time.deltaTime;
+        MovePlayer();
+    }
 
+    private void MovePlayer()
+    {
+        float verticalMovement = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+        float horizontalMovement = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
         transform.Translate(horizontalMovement, 0, verticalMovement);
-
     }
 }
