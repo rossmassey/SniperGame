18a4099be4b0f7f3108c89e2885fa3f74468af64
Ross Massey <rmassey@ucsd.edu>
2019-03-29 16:12:11 -0700

    Add movement

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Controls player movement
+ * 
+ * Thanks to Holistic3d on YouTube for their tutorial
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float playerSpeed = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // lock cursor to window and hide
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalMovement = 
+            Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+        float horizontalMovement = 
+            Input.GetAxis("Horizontal") * playerSpeed  * Time.deltaTime;
+
+        transform.Translate(horizontalMovement, 0, verticalMovement);
+
+    }
+}
