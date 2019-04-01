675db18f3f75ece983e6407d7d92551a589d13d8
Ross Massey <rmassey@ucsd.edu>
2019-04-01 09:23:37 -0700

    Add enemy and weapon

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float maxHealth = 100.0f;
+    float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public float HealthPercentage()
+    {
+        return currentHealth / maxHealth;
+    }
+
+    public void DamageHealth(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - amount, 0, maxHealth);
+        if (currentHealth == 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public string name;
+    public float damage;
+    public int clipSize;
+
+    int currentClipAmount;
+    int ammunition;
+
+    public bool Fire()
+    {
+        return false;
+    }
+
+    void DamageEnemy(Enemy enemy)
+    {
+
+    }
+
+}
