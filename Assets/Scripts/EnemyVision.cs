be1ad56119db40f75f10558aed8d4dce375f6c75
Ross Massey <rmassey@ucsd.edu>
2019-04-26 17:44:27 -0700

    Start enemy vision

---
+++ Assets/Scripts/EnemyVision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Spotted player");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Lost sight player");
+        }
+    }
+}
